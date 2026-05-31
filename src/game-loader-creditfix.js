fafc111846ede1b1206f8f5671c31c15bb94a18c
BT <tekwanib7@gmail.com>
2026-05-31 19:17:15 +0100

    Make bottom maker credit first and fully bold

--- src/game-loader-creditfix.js
+++ src/game-loader-creditfix.js
@@ -3,6 +3,11 @@
   let loader = await response.text();
 
   loader = loader.replace(
+    "ctx.font = '500 9px Montserrat, Arial, sans-serif';\\n    ctx.fillText('Copyright: Paramount Pictures, Warner Bros. Entertainment, and Legendary Pictures', W / 2, H - 42);\\n    ctx.fillText('Maker: QuantumPikachu (Reddit)', W / 2, H - 27);",
+    "ctx.font = '700 9px Montserrat, Arial, sans-serif';\\n    ctx.fillText('Maker: QuantumPikachu (Reddit)', W / 2, H - 42);\\n    ctx.font = '500 9px Montserrat, Arial, sans-serif';\\n    ctx.fillText('Copyright: Paramount Pictures, Warner Bros. Entertainment, and Legendary Pictures', W / 2, H - 27);"
+  );
+
+  loader = loader.replace(
     "ctx.fillText('Copyright: Paramount Pictures, Warner Bros. Entertainment, and Legendary Pictures', W / 2, H - 42);\\n    ctx.fillText('Maker: QuantumPikachu (Reddit)', W / 2, H - 27);",
     "ctx.font = '700 9px Montserrat, Arial, sans-serif';\\n    ctx.fillText('Maker: QuantumPikachu (Reddit)', W / 2, H - 42);\\n    ctx.font = '500 9px Montserrat, Arial, sans-serif';\\n    ctx.fillText('Copyright: Paramount Pictures, Warner Bros. Entertainment, and Legendary Pictures', W / 2, H - 27);"
   );
